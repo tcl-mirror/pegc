5351975669a47f966d82b9b6e8552b37b11d9488
http://wanderinghorse.net/home/stephan/ <http://wanderinghorse.net/home/stephan/>
2008-09-26 15:13:29 +0000

    corrected bogus double definitions of hastable_iterator_{key,value}()

--- src/hashtable_itr.h
+++ src/hashtable_itr.h
@@ -34,20 +34,14 @@ hashtable_iterator(hashtable *h);
 /* hashtable_iterator_key
  * - return the value of the (key,value) pair at the current position */
 
-extern inline void *
-hashtable_iterator_key(hashtable_itr *i)
-{
-    return i->e->k;
-}
+extern void *
+hashtable_iterator_key(hashtable_itr *i);
 
 /*****************************************************************************/
 /* value - return the value of the (key,value) pair at the current position */
 
-extern inline void *
-hashtable_iterator_value(hashtable_itr *i)
-{
-    return i->e->v;
-}
+extern void *
+hashtable_iterator_value(hashtable_itr *i);
 
 /*****************************************************************************/
 /* advance - advance the iterator to the next element
